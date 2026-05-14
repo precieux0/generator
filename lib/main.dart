606459ac689c5bc433ccb7ea0cf4f853a25078f8
precieux0 <jmakumbusela@gmail.com>
2026-05-14 04:19:05 +0100

    Add proper Flutter project structure for l10n generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/app_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Container(),
+    );
+  }
+}
